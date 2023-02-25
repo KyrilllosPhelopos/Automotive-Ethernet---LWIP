c2af3d380a267107d44b8ae79b5dcf38056f85e6
Kyrillos Sawiris <kyrillos.sawiris99@gmail.com>
2023-02-26 00:36:57 +0200

    netconn client send image from init function

--- Netconn APIs/tcp client/Src/tcp_client.c
+++ Netconn APIs/tcp client/Src/tcp_client.c
@@ -5,10 +5,6 @@
  *      Author: Kyrillos Sawiris
  */
 
-
-
-
-
 #include "lwip/opt.h"
 
 #include "lwip/api.h"
@@ -23,7 +19,10 @@ static ip_addr_t *addr, dest_addr;
 static unsigned short port, dest_port;
 char msgc[100];
 char smsgc[200];
-int indx = 0;
+
+char message [10] = "hello";
+char* ProgramToSend = (char*)0x10000000;
+//int indx = 0;
 
 // Function to send the data to the server
 void tcpsend (char *data);
@@ -78,6 +77,12 @@ static void tcpinit_thread(void *arg)
 							// semaphore must be taken before accessing the tcpsend function
 							//sys_arch_sem_wait(&tcpsem, 500);
 
+							if(strcmp(msgc , "SEND")==0)
+							{
+							  sys_thread_new("tcpinit_thread", tcpinit_thread, NULL, DEFAULT_THREAD_STACKSIZE,osPriorityNormal);
+
+							}
+
 							// send the data to the TCP Server
 							tcpsend (smsgc);
 
@@ -114,25 +119,82 @@ void tcpsend (char *data)
 }
 
 
-static void tcpsend_thread (void *arg)
+static void tcp_SendProgram (void *arg)
 {
-	for (;;)
-	{
-		sprintf (smsgc, "index value = %d\n", indx++);
-	// semaphore must be taken before accessing the tcpsend function
-		//sys_arch_sem_wait(&tcpsem, 500);
-		// send the data to the server
-		tcpsend(smsgc);
-		osDelay(500);
-	}
+
+	tcpsend(ProgramToSend);
+
+	//	for(int i=0;i<3000;i++)
+	//		{
+	//			ProgramToSend[i] = 'a';
+	//		}
+	//	for (;;)
+	//	{
+	//		//sprintf (smsgc, "index value = %d\n", indx++);
+	//	// semaphore must be taken before accessing the tcpsend function
+	//		//sys_arch_sem_wait(&tcpsem, 500);
+	//		// send the data to the server
+	//
+	//		tcpsend(ProgramToSend);
+	//		//osDelay(500);
+	//	}
+	osThreadId_t id;
+	id = osThreadGetId();
+	osThreadTerminate(id);
 }
 
+
+static void tcp_SendMessage (void *arg)
+{
+	tcpsend(message);
+	osThreadId_t id;
+	id = osThreadGetId();
+	osThreadTerminate(id);
+
+}
+
+void tcp_ReseveMessage (void *arg )
+{
+	if (netconn_recv(conn, &buf) == ERR_OK)
+	{
+		do
+		{
+
+			strncpy (msgc, buf->p->payload, buf->p->len);   // get the message from the server
+
+			// Or modify the message received, so that we can send it back to the server
+			sprintf (smsgc, "\"%s\" was sent by the Server\n", msgc);
+
+			// semaphore must be taken before accessing the tcpsend function
+			//sys_arch_sem_wait(&tcpsem, 500);
+
+			// send the data to the TCP Server
+			tcpsend (smsgc);
+
+			memset (msgc, '\0', 100);  // clear the buffer
+		}
+		while (netbuf_next(buf) >0);
+
+		netbuf_delete(buf);
+	}
+
+	//	id = osThreadGetId();
+	//	osThreadTerminate(id);
+}
 
 
 
 void tcpclient_init (void)
 {
+	for(int i=0;i<3000;i++)
+	{
+		ProgramToSend[i] = 'a';
+	}
 	//sys_sem_new(tcpsem, 0);  // the semaphore would prevent simultaneous access to tcpsend
+
 	sys_thread_new("tcpinit_thread", tcpinit_thread, NULL, DEFAULT_THREAD_STACKSIZE,osPriorityNormal);
-	sys_thread_new("tcpsend_thread", tcpsend_thread, NULL, DEFAULT_THREAD_STACKSIZE,osPriorityNormal);
+	//sys_thread_new("tcpSendProgram_thread", tcp_SendProgram, NULL, DEFAULT_THREAD_STACKSIZE,osPriorityNormal);
+	//sys_thread_new("tcp_ReseveMessage", tcp_ReseveMessage, NULL, DEFAULT_THREAD_STACKSIZE,osPriorityNormal);
+	//sys_thread_new("tcp_SendMessage", tcp_SendMessage, NULL, DEFAULT_THREAD_STACKSIZE,osPriorityNormal);
+
 }
